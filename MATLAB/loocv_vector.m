546dfc87813e57bde7574c8d0bd47740e81866a3
Matteo Caoduro <matteo.caoduro@gmx.com>
2021-02-17 07:29:06 +0100

    Updated

--- MATLAB/loocv_vector.m
+++ MATLAB/loocv_vector.m
@@ -1,4 +1,4 @@
-function [E, U, Ns, a, C] = loocv_vector(A, f)
+function [E, U, Ns2, a, C] = loocv_vector(A, f)
 
 I = eye(size(A));     % Identity matrix
 Y = A\[I, f];         % ‘C’ and ‘a’ (alpha) side by side
@@ -6,6 +6,6 @@ C = Y(:, 1:(end-1));  % Inverse of ‘A’
 a = Y(:, end);        % Interpolation coefficients (alpha)
 E = a./diag(C);       % Cross-validation vector
 U = E.*a;             % “Cross-validation” of norms
-Ns = a'*f;            % Squared native space norm of ‘s’
+Ns2 = a'*f;           % Squared native space norm of ‘s’
 
 end
--- MATLAB/loocv_vector_pinv.m
+++ MATLAB/loocv_vector_pinv.m
@@ -1,10 +1,10 @@
-function [E, U, Ns, a, C] = loocv_vector_pinv(A, f)
+function [E, U, Ns2, a, C] = loocv_vector_pinv(A, f)
 
 I = eye(size(A));     % Identity matrix
 C = pinv(A);          % “Stable” inverse of ‘A’
 a = C*f;              % Interpolation coefficients (alpha)
 E = a./diag(C);       % Cross-validation vector
 U = E.*a;             % “Cross-validation” of norms
-Ns = a'*f;            % Squared native space norm of ‘s’
+Ns2 = a'*f;           % Squared native space norm of ‘s’
 
 end
