28c6e4cb47f8b7a6e598f9a722e7f77f950d1a3b
Matteo Caoduro <matteo.caoduro@gmx.com>
2021-03-07 09:52:29 +0100

    Corrections

--- MATLAB/loocv_vector_pinv.m
+++ MATLAB/loocv_vector_pinv.m
@@ -1,6 +1,5 @@
 function [E, U, Ns2, a, C] = loocv_vector_pinv(A, f)
 
-I = eye(size(A));     % Identity matrix
 C = pinv(A);          % “Stable” inverse of ‘A’
 a = C*f;              % Interpolation coefficients (alpha)
 E = a./diag(C);       % Cross-validation vector
